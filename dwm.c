6496afbf830b8abce74280a09051ed9e34af738f
Edvard <elsberg1995@protonmail.com>
2020-09-26 14:30:55 +0200

    make statuscmd and statuscolor work together

--- dwm.c
+++ dwm.c
@@ -452,7 +452,7 @@ buttonpress(XEvent *e)
 			char ch;
 			statuscmdn = 0;
 			while (text[++i]) {
-				if ((unsigned char)text[i] < ' ') {
+				if ((unsigned char)text[i] < '\x10') {
 					ch = text[i];
 					text[i] = '\0';
 					x += TEXTW(text) - lrpad;
