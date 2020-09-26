064e1729535b7e583a5995cfd0da4d94033fbb84
Edvard <elsberg1995@protonmail.com>
2020-09-26 14:33:27 +0200

    changed named of termnial for swallow to work may be

--- config.def.h
+++ config.def.h
@@ -47,7 +47,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 	{ NULL,		"spterm",	NULL,		   SPTAG(0),   1,		  -1,          1,        -1 },
 	{ NULL,		"spfm",		NULL,		   SPTAG(1),   1,		  -1,          1,        -1 },
